81afffd42f2eabcb672ea4b015038c71c185dc0f
Bildan Jauhary <bildanjauhary@gmail.com>
2026-05-18 10:20:46 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function arrEvenOdd(arr, status){
+    let isEven = arr.every((currVal) => currVal % 2 === 0)
+    let isOdd = arr.every((currVal) => currVal % 2 !== 0)
+
+    if(status === 'Even'){
+        return isEven
+    } else if(status === 'Odd') {
+        return isOdd
+    } 
+    
+    return false
+    
+}
+
+console.log(arrEvenOdd([1, 1, 1], 'Odd'))
